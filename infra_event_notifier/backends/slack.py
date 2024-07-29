dbf6de40511e039f29680203e441df73a2155a4f
Brian Lou <brian.lou@sentry.io>
2024-07-29 16:34:11 -0700

    Add slack integration with eng-pipes

--- infra_event_notifier/backends/slack.py
+++ infra_event_notifier/backends/slack.py
@@ -1,3 +1,45 @@
-def send_notification(title: str, text: str, slack_api_key: str) -> None:
-    # TODO: implement
-    pass
+import json
+import urllib.request
+from urllib.error import HTTPError
+
+
+def send_notification(
+    title: str,
+    text: str,
+    channel_id: str,
+    eng_pipes_key: str,
+    eng_pipes_url: str,
+) -> None:
+    """
+    Sends an event to Slack via eng-pipes.
+    See: https://github.com/getsentry/eng-pipes
+
+    :param title: Title of Slack Message
+    :param text: Body of Slack Message
+    :param channel_id: ID of the Slack channel to send the message to
+    :param eng_pipes_key: Secret Key used to HMAC sign request
+    :param eng_pipes_url: Full URL for eng-pipes slack webhooks
+    """
+    # API docs: https://docs.datadoghq.com/api/latest/events/#post-an-event
+    payload = {
+        "source": "infra-event-notifier",
+        "title": title,
+        "body": text,
+        "channel": channel_id,
+    }
+    json_data = json.dumps(payload)
+    data = json_data.encode("utf-8")
+    req = urllib.request.Request(eng_pipes_url, data=data)
+    # TODO: Actually do HMAC signing
+    req.add_header("x-hmac-key", eng_pipes_key)  # currently unused
+    req.add_header("Content-Type", "application/json; charset=utf-8")
+    with urllib.request.urlopen(req) as response:
+        status = response.status
+        if status > 202:
+            raise HTTPError(
+                url=response.url,
+                code=status,
+                msg=f"Recieved {status} response from Datadog",
+                hdrs=response.headers,
+                fp=None,
+            )
--- infra_event_notifier/slack_notifier.py
+++ infra_event_notifier/slack_notifier.py
@@ -2,15 +2,22 @@ from infra_event_notifier.backends.slack import send_notification
 
 
 class SlackNotifier:
-    def __init__(self, slack_api_key: str) -> None:
-        self.slack_api_key = slack_api_key
+    def __init__(self, eng_pipes_key: str, eng_pipes_url: str) -> None:
+        self.eng_pipes_key = eng_pipes_key
+        self.eng_pipes_url = eng_pipes_url
 
-    def send(self, title: str, body: str) -> None:
+    def send(self, title: str, body: str, channel_id: str) -> None:
         """
-        Sends the notification
+        Sends the message to slack
         """
-        # TODO: implement
-        if self.slack_api_key is not None and title and body:
-            send_notification(
-                title=title, text=body, slack_api_key=self.slack_api_key
-            )
+        assert self.eng_pipes_key is not None, "Missing Eng-Pipes Signing Key"
+        assert self.eng_pipes_url is not None, "Missing Eng-Pipes API URL"
+        assert channel_id != "", "Missing Slack Channel ID"
+
+        send_notification(
+            title=title,
+            text=body,
+            channel_id=channel_id,
+            eng_pipes_key=self.eng_pipes_key,
+            eng_pipes_url=self.eng_pipes_url,
+        )
