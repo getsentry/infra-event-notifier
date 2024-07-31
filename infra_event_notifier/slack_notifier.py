701100df62ff5e63112b0156e5ab595e4f09de17
Brian Lou <brian.lou@sentry.io>
2024-07-31 09:58:07 -0700

    Add minor checks

--- infra_event_notifier/slack_notifier.py
+++ infra_event_notifier/slack_notifier.py
@@ -2,6 +2,11 @@ from infra_event_notifier.backends.slack import send_notification
 
 
 class SlackNotifier:
+    """
+    Class that supports sending Slack messages.
+    A URL for eng-pipes and eng-pipes secret key are required.
+    """
+
     def __init__(self, eng_pipes_key: str, eng_pipes_url: str) -> None:
         self.eng_pipes_key = eng_pipes_key
         self.eng_pipes_url = eng_pipes_url
@@ -12,7 +17,12 @@ class SlackNotifier:
         """
         assert self.eng_pipes_key is not None, "Missing Eng-Pipes Signing Key"
         assert self.eng_pipes_url is not None, "Missing Eng-Pipes API URL"
-        assert channel_id != "", "Missing Slack Channel ID"
+        assert channel_id is not None, "Missing Slack Channel ID"
+
+        if (
+            self.eng_pipes_url == "" or channel_id == ""
+        ):  # For tests, to avoid sending them out
+            return
 
         send_notification(
             title=title,
@@ -21,3 +31,11 @@ class SlackNotifier:
             eng_pipes_key=self.eng_pipes_key,
             eng_pipes_url=self.eng_pipes_url,
         )
+
+
+# Remove before committing
+if __name__ == "__main__":
+    notif = SlackNotifier(
+        "", "http://localhost:3000/metrics/infra-event-notifier/webhook"
+    )
+    notif.send("Test Title", "Test Body__", "C07EKV0T2E6")
